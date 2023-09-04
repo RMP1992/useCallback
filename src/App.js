f015df145f50103dc48d122f022af1c270f81b68
RMP1992 <45944114+RMP1992@users.noreply.github.com>
2023-09-04 10:28:34 +0100

    implemented useCallback

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ const App = () => {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
 
-  const getItems = () => {
+  const getItems = useCallback(() => {
     return [number, number + 1, number + 2];
-  };
+  }, [number]);
 
   const theme = {
     backgroundColor: dark ? "#333" : "#fff",
